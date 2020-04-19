7ca80b07783cf283ed5f7b9bb7500e6f62c0685e
Bmercado17 <brnrdsnchz17@gmail.com>
2020-04-18 17:32:51 -0700

    fourth changes

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
-   $(document).click(function(){
-       $('.button').switchClass('button', "otherClass", 1000);
-   }) 
+   
+    (function startQuiz (){
+
+        this.loadQuiz = function(){
+            $(".panel-one h1").show('drop', 1000,function(){
+                $(".start-quiz").addClass("started", 800);
+            });
+            $(".start-quiz").on("click",function(){
+                showPanel(1)
+            })
+        }
+        this.showPanel = function(position){ //the position is to show the current panel
+            var current = $('div[data-panel="'+ (position -1) +'"]') //this is to hide and move to the next panel
+            current.find(".wrapper").animate({left:"-100px", opacity:0},800,function(){
+                
+                //hide first panel
+                current.addClass("hidden");
+                //show second panel and so on.
+            var next = $('div[data-panel="' + position + '"]')
+            next.removeClass("hidden");
+
+            })
+        };
+        
+        loadQuiz();
+
+    })();
    
 });
